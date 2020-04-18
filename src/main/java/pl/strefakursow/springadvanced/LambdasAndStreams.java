03746caad2921de99c7b0ab531ddc24f39e993eb
LukaszP12 <lukaszp4@onet.eu>
2020-04-18 18:32:49 +0200

    9th commit including LambdasAndStreams

--- src/main/java/pl/strefakursow/springadvanced/LambdasAndStreams.java
+++ src/main/java/pl/strefakursow/springadvanced/LambdasAndStreams.java
@@ -8,16 +8,16 @@ public class LambdasAndStreams {
 
     public static void main(String[] args) {
 
-      /*  Thread thread = new Thread(() -> System.out.println("Informacja z osobnego wątku"));
+      /*Thread thread = new Thread(() -> System.out.println("Informacja z osobnego wątku"));
         Thread thread1 = new Thread(() -> System.out.println("Informacja z drugiego wątku"));
 
         thread.start();
         thread1.start();
         System.out.println("Informacja z metody main"); */
 
-      List<Integer> numbers = Arrays.asList(52,17,44,67,98,60,21,18,57,102);
+        List<Integer> numbers = Arrays.asList(52, 17, 44, 67, 98, 60, 21, 18, 57, 102);
 
-      int result = 0;
+ /*     int result = 0;
 
         System.out.println(
                 numbers.stream()
@@ -25,7 +25,15 @@ public class LambdasAndStreams {
                         .mapToInt(x -> x * 2)
                         .sum()
         );
+    } */
 
+
+        // Reduce() allows us to get a single element from source
+        System.out.println(numbers.stream().reduce(0, (subtotal, element) -> subtotal + element));
+
+        // Collect
+        List<Integer> list = numbers.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+        System.out.println(list);
 
     }
 
